9b66bf9b0e237dab189c1e2c92887d61886b80af
godeline <godelineboy@gmail.com>
2021-12-28 20:15:44 +0100

    rectif challenge thymeleaf

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import com.example.thymeleaf.model.Doctor;
 
 @Controller
 public class DoctorController{
@@ -12,9 +13,9 @@ public class DoctorController{
     public String doctor(@RequestParam(required=false, defaultValue="0") int number,
                          @RequestParam(required=false, defaultValue="John Smith") String name, Model model) {
 	
-		model.addAttribute("name", name);
-    	model.addAttribute("number", number);
+		Doctor doctorId = new Doctor(number, name);
+		model.addAttribute("doctor", doctorId);
     	
-        return "doctor";
+        return "doctor.html";
     }
 }
